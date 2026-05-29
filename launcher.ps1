e406b16492fc3b1f5ba06901f0719424918f3118
Cleiton Souza <cleitonsouza.work@gmail.com>
2026-05-29 15:34:25 -0300

    Update BAT file URL in launcher.ps1

--- launcher.ps1
+++ launcher.ps1
@@ -2,7 +2,7 @@
 $ErrorActionPreference = "SilentlyContinue"
 
 # URL do seu arquivo BAT (Cole o link RAW aqui dentro das aspas)
-$batUrl = "bit.ly/eagle-toolnet"
+$batUrl = "bit.ly/eagle-toolnetcmd"
 
 # Define que o BAT sera salvo na pasta temporaria do Windows
 $batPath = "$env:TEMP\EagleTech_ToolNet.bat"
