7fc531225c9e50e3c5f2af944a11db5ae140a215
Cleiton Souza <cleitonsouza.work@gmail.com>
2026-05-29 15:40:09 -0300

    Fix URL format for BAT file in launcher.ps1

--- launcher.ps1
+++ launcher.ps1
@@ -1,5 +1,5 @@
 # URL do seu arquivo BAT (Confirme se comeca com raw.githubusercontent.com)
-$batUrl = "https://raw.githubusercontent.com/bigueiraa/Scripts-EagleTech-ToolNet/refs/heads/main/launcher.ps1"
+$batUrl = "raw.githubusercontent.com/bigueiraa/Scripts-EagleTech-ToolNet/refs/heads/main/launcher.ps1"
 $batPath = "$env:TEMP\EagleTech_ToolNet.bat"
 
 Write-Host "Iniciando Eagle Tech ToolNet..." -ForegroundColor Cyan
